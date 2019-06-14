d036c5ee99ae1d859a23d52aeab6a89817326299
ydying <1778679036@qq.com>
2019-06-14 14:35:15 +0800

    backup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Div(props) {
+    const { style } = props
+    function onMouseEnter(num) {
+        console.log('onMouseEnter:' + num)
+    }
+
+    return <div style={style} onMouseEnter={e => onMouseEnter(4)}></div>
 }
 
-export default App;
+function App() {
+
+    function onMouseEnter(num) {
+        console.log('onMouseEnter:' + num)
+    }
+
+    const elems = [
+        {
+            id: '123',
+            style: {
+                width: 200,
+                height: 200,
+                backgroundColor: '#09c'
+            }
+        }
+    ]
+
+    return (
+        <div className="App">
+            <div className="editor">
+                {elems.map(item => {
+                    return <Div style={item.style} />
+                })}
+                <div style={{width: 500,height: 500,backgroundColor: '#999'}} onMouseEnter={e => onMouseEnter(3)}>
+                    <div style={{width: 100,height: 100,backgroundColor: '#666'}} onMouseEnter={e => onMouseEnter(1)}></div>
+                    <div style={{width: 100,height: 100,backgroundColor: '#666'}} onMouseEnter={e => onMouseEnter(2)}></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
